8b13cfe3c7a6d5e55de58a7945aefa4e14ec6107
nicentra <mikesch1993@gmail.com>
2017-05-06 20:34:21 +0200

    .

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,10 +14,12 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+        System.out.println("Hello, World!");
     }
 
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
